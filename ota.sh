a90c9c6ca0307b8c6e051494fd6828b30183b50a
Dennis Hoang <hvdien@gmail.com>
2023-11-28 23:41:01 +0800

    OTA: Upload GSI ROM for OTA testing
    Signed-off-by: Dennis Hoang <hvdien@gmail.com>

---
+++ ota.sh
@@ -0,0 +1,11 @@
+{
+	"version": "v2023.11.28",
+	"date": "1701183649",
+	"variants": [
+		{
+			"name": "treble_arm64_bvN",
+			"size": "1231700440",
+			"url": "https://github.com/ponces/treble_build_aosp/releases/download/v2023.11.28/"
+		}
+	]
+}
