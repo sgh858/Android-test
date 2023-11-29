2806b2a71859df8770d0570097fb720c8ada4629
Dennis Hoang <hvdien@gmail.com>
2023-11-29 16:48:04 +0800

    OTA: Change ota.sh script to create the json and xz file automatically.
    Signed-off-by: Dennis Hoang <hvdien@gmail.com>

--- ota.sh
+++ ota.sh
@@ -1,11 +1,33 @@
-{
-	"version": "v2023.11.28",
-	"date": "1701183649",
-	"variants": [
-		{
-			"name": "treble_arm64_bvN",
-			"size": "1231700440",
-			"url": "https://github.com/ponces/treble_build_aosp/releases/download/v2023.11.28/"
-		}
-	]
-}
+OTA=$HOME/Android-test
+OUT=$HOME/android-phh/out/target/product/tdgsi_arm64_ab
+
+version="$(date +v%Y.%m.%d)"
+
+# Get the time when the GSI ROM is built
+timestamp=$(cat $OUT/system/build.prop | grep ro.build.date.utc | awk -F "=" '{print $2}')
+
+[ -z "timestamp" ] && timestamp="$(date +%s)"
+name="treble_arm64_bvN"
+fileimg="$OUT/system.img"
+filexz="$OTA/$name-13.0-$timestamp.img.xz"
+cp $fileimg $HOME/builds/system-$timestamp.img
+
+echo
+
+echo "--> Generating XZ file"
+# xz -cv $fileimg -T0 > $filexz
+
+echo "--> Generating OTA json file"
+
+size=$(wc -c $filexz | awk '{print $1}')
+url="https://github.com/sgh858/Android-test/raw/main/$name-13.0-$timestamp.img.xz"
+
+json="{\n\t\"version\": \"$version\",\n\t\"date\": \"$timestamp\",\n\t\"variants\": ["
+json="${json}\n\t\t{\n\t\t\t\"name\": \"$name\",\n\t\t\t\"size\": \"$size\",\n\t\t\t\"url\": \"$url\"\n\t\t}"
+json="${json%?\n}\n\t]\n}"
+
+echo -e "$json"
+
+echo -e "$json" > $OTA/ota.json
+
+echo
